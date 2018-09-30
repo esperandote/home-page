39588d8c564a60488bf3fbf3f285d7942057d505
esperandote <34682468+esperandote@users.noreply.github.com>
2018-09-30 11:25:44 +0800

    Add files via upload

--- js/homepage.js
+++ js/homepage.js
@@ -5,39 +5,17 @@ localStorage.imgnum is the value of imgshowing when you closed the page last tim
 */
 var bgimg=document.getElementById('bgimg');
 bgimg.src='images/bg'+(imgshowing+1)+'.jpg';
-//animation where 'someimg' gets dark
-function dark(someimg){
-  var o=1;
-  function darkstep(someimg){
-    o-=0.2;
-    someimg.style.opacity=o;
-    if(o<0.05){
-      clearInterval(t);
-    }
-  }
-  var t=setInterval(function(){darkstep(someimg)},60);
-}
-//animation where 'someimg' gets light
-function light(someimg){
-  var o=0;
-  function lightstep(someimg){
-    o+=0.2;
-    someimg.style.opacity=o;
-    if(o>=1){
-      clearInterval(t);
-    }
-  }
-  var t=setInterval(function(){lightstep(someimg)},60);
-}
 //switch to the next image
 function nextimg(){
-  dark(bgimg);
+  bgimg.style.animation='nextimg 1s';
   setTimeout(function(){
     if(imgshowing==11){imgshowing=-1;}
     imgshowing+=1;
     bgimg.src='images/bg'+(imgshowing+1)+'.jpg';
-    light(bgimg);
-  },300);
+  },400);
+  setTimeout(function(){
+    bgimg.style.animation='';
+  },1000);
 }
 //stores the value of imgshowing in localStorage.imgnum
 window.onunload=function(){
